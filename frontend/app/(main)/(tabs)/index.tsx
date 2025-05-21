9b7e3e8fe0acb85743f88706d78106dbef910433
tricia <triciachew18@gmail.com>
2025-05-21 19:59:56 +0800

    feat: home screen UI with progress stats, upcoming goals and daily quote

---
+++ frontend/app/(main)/(tabs)/index.tsx
@@ -0,0 +1,131 @@
+import { StyleSheet, ScrollView, View, Text,TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { s, ms, vs } from 'react-native-size-matters';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import DonutChart from '@/components/AnimatedDonutChart';
+import AnimatedTextInput from '@/components/AnimatedTextInput';
+
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <ThemedView style={{flex: 1}}>
+
+            <View style={styles.mainContainer}>
+              <View style={styles.titleContainer}>
+                <ThemedText type="title">Hello, (user)!</ThemedText>
+                
+                <View style={styles.iconContainer}>
+                  <TouchableOpacity>
+                    <Ionicons name="notifications" size={ms(28)} color="#66cdaa" />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity>
+                    <FontAwesome name="user-circle" size={ms(50)} color="#6a5acd" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              
+              <View style={styles.subContainer}>
+                <Text style={styles.header}>My Progress</Text>
+
+                <View style={styles.progressStats}>
+                  <View style={styles.donutContainer}>
+                    <DonutChart value={50} radius={ms(60)} strokeWidth={ms(25)}/>
+                  </View>
+
+                    <View style={styles.progressTextContainer}>
+                      <AnimatedTextInput value={10} textColor='white' size={RFValue(25)}/>
+                      <Text style={styles.progressText}>OUT OF</Text>
+                      <AnimatedTextInput value={20} textColor='white' size={RFValue(25)}/>
+                      <Text style={styles.progressText}>COMPLETED</Text>
+                    </View>
+                  </View>
+              </View>
+
+              <View style={[{height: vs(200)}]}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                  <View style={[styles.subContainer, { flex: 1 }]}>
+                      <Text style={styles.header}>Upcoming</Text>
+                        <View>
+                          <Text style={[styles.smallText, {color: 'white'}]}>
+                            You have no upcoming goals. Set one now!
+                          </Text>
+                        </View>
+                    </View>
+                </ScrollView>
+              </View>
+
+
+              <View style={styles.quoteContainer}>
+                <ThemedText style={styles.smallText}>
+                  Twenty years from now you will be more disappointed by the things you didn't do than by the ones you did do. — Mark Twain
+                </ThemedText>
+              </View>
+            </View>
+
+          </ThemedView>
+        </ScrollView>
+    </SafeAreaView>
+  )}
+
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingHorizontal: s(20),
+    paddingTop: vs(15),
+    gap: s(14),
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    gap: s(20),
+    alignItems: 'center'
+  },
+  subContainer: {
+    alignItems: 'center',
+    paddingVertical: vs(15),
+    paddingHorizontal: s(10),
+    backgroundColor: '#6a5acd',
+    gap: vs(10),
+  },
+  progressStats: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: s(25),
+  },
+  donutContainer: {
+    alignItems: 'center',
+  },
+  progressTextContainer: {
+    alignItems: 'center',
+  },
+  progressText: {
+    lineHeight: ms(30),
+    color: 'white',
+  },
+  quoteContainer: {
+    paddingVertical: vs(15),
+    paddingHorizontal: s(10),
+    backgroundColor: 'rgba(102, 205, 170, 0.7)',
+  },
+  header: {
+    color: 'white',
+    fontSize: RFValue(20),
+    fontWeight: '600',
+    textAlign: 'center',
+  },
+  smallText: {
+    fontSize: RFValue(13),
+  },
+});
