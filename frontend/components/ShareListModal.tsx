9531dfbf7c8b828bd10631fcb992dccdd90391bd
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-05-29 22:15:23 +0800

    feat: refine modal UI styling
    - Improved spacing between modal components
    - Updated 'Add people' TextInput with focus styling (onFocus/ onBlur)
    - Added responsive fontsizing and adjusted fontWeight of text components
    - Adjusted border radius, position and colour of close button

--- frontend/components/ShareListModal.tsx
+++ frontend/components/ShareListModal.tsx
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useState } from "react";
+import { RFValue } from "react-native-responsive-fontsize";
 
 interface User {
   // to add profile icon
@@ -17,11 +18,11 @@ interface User {
 
 const renderFlatlistItem = ({ item }: { item: User }) => {
   return (
-    <View style={{ flexDirection: "row" }}>
+    <View style={styles.profile}>
       <FontAwesome name="user-circle" size={20} color="black" />
 
       <TouchableOpacity>
-        <Text>{item.username}</Text>
+        <Text style={{ fontSize: RFValue(12) }}>{item.username}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -39,25 +40,38 @@ export default function ShareListModal({
   onClose,
 }: CustomModalProps) {
   const [username, setUsername] = useState("");
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     <Modal visible={visible} transparent={true} animationType="fade">
       <View style={styles.modalContent}>
         <View style={styles.card}>
-          <Text>Share List</Text>
+          <Text style={{ fontSize: RFValue(16), fontWeight: "bold" }}>
+            Share List
+          </Text>
           <TextInput
             placeholder="Add people"
             value={username}
             onChangeText={setUsername}
             enterKeyHint="search"
-          ></TextInput>
-          <Text>People with access</Text>
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            style={[styles.textInput, isFocused && styles.inputWrapperFocused]}
+          />
+          <Text style={styles.withAcessText}>People with access</Text>
           <FlatList
             data={data}
             renderItem={renderFlatlistItem}
             keyExtractor={(item) => item.username}
           />
-          <View>
-            <TouchableOpacity style={[styles.button, {}]} onPress={onClose}>
+          <View style={{ alignItems: "flex-end" }}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                onClose();
+                setIsFocused(false);
+              }}
+            >
               <Text>Close</Text>
             </TouchableOpacity>
           </View>
@@ -80,11 +94,33 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderRadius: 8,
     elevation: 3,
+    gap: 8,
   },
   button: {
-    alignItems: "flex-end",
-    borderRadius: 8,
-    width: "50%",
-    backgroundColor: "rgba(0,0,0,0.1)",
+    alignItems: "center",
+    borderRadius: 20,
+    width: "40%",
+    backgroundColor: "#c6e1dc",
+    padding: 8,
+  },
+  withAcessText: {
+    fontWeight: "bold",
+    fontSize: RFValue(13),
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: "#ccc",
+    backgroundColor: "#f9f9f9",
+    paddingHorizontal: 15,
+    marginBottom: 12,
+  },
+  inputWrapperFocused: {
+    borderColor: "#03acc1", // Highlight color when focused
+  },
+  profile: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
   },
 });
