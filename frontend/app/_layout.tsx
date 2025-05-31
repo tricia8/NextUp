fac6bea415dc5c5e1e8ca6b393fecde46c1cb6ef
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-05-31 14:03:23 +0800

    refactor: delegate route control to nested layouts using Slot

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,59 +1,14 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import { AuthProvider } from "@/context/AuthContext";
-import { useColorScheme } from "@/hooks/useColorScheme";
 import FlashMessage from "react-native-flash-message";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
     <AuthProvider>
       <SafeAreaProvider>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen
-              name="(auth)/signup"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(auth)/login"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(auth)/forgot-password"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(main)/(tabs)"
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="(main)/new-sublist"
-              options={{ title: "New Sublist" }}
-            />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </ThemeProvider>
+        <Slot />
       </SafeAreaProvider>
       <FlashMessage position="top" />
     </AuthProvider>
