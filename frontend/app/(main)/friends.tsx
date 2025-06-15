a3ed617b35cc1d571cc884dbb190bcf428f8ddac
tricia <triciachew18@gmail.com>
2025-06-16 00:41:09 +0800

    refactor: update friends screen to use abstracted UserSearch component

--- frontend/app/(main)/friends.tsx
+++ frontend/app/(main)/friends.tsx
@@ -1,113 +1,66 @@
-import React from 'react';
-import { StyleSheet, ScrollView, View, TextInput, TouchableOpacity, useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { s, ms, vs } from 'react-native-size-matters';
-import { ThemedText } from '@/components/ThemedText';
+import { ScrollView, StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
-import { LegendList } from '@legendapp/list';
-import { FontAwesome } from '@expo/vector-icons';
+import { vs } from 'react-native-size-matters';
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from "@/firebase/firebaseConfig";
+import { getAuth } from 'firebase/auth';
+import UserSearch from '@/components/UserSearch';
+import { User } from '@/types/user';
 
 
 
-const friends = [
-        { id: 0, username: 'bp099' },
-        { id: 1, username: 'ef32' },
-    ];
+const auth = getAuth();
+let data: User[];
 
-export default function FriendsScreen() {
-    const [search, setSearch] = React.useState('');
-    const [filteredFriends, setFriends] = React.useState(friends);
-    const colorScheme = useColorScheme();
-    const styles = makeStyles(colorScheme);
+export default function FriendsList() {
+  const [currentUserId, setCurrentUserId] = useState<string | null>(null);
 
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setCurrentUserId(user.uid);
+    }
+  }, []);
 
-    type Friend = {
-      id: number,
-      username: string,
+  useEffect(() => {
+    if (!currentUserId) {
+      return;
     }
 
-    
-
-
-    const filterData = (text: string) => {
-      const formattedQuery = text.toLowerCase();
-      const filtered = friends.filter((item: Friend) => {
-        return item.username.toLowerCase().includes(formattedQuery);
-      });
-      setFriends(filtered);
-      setSearch(text);
-    };
-
-
-    function renderItem({item}: {item: Friend}) {
-          return (
-              <TouchableOpacity style={styles.friendsContainer}>
-                  <FontAwesome name="user-circle-o" size={ms(40)} color="#7b68ee" />
-                  <ThemedText style={{fontSize: RFValue(14)}}>{item.username}</ThemedText>
-              </TouchableOpacity>
-          )
+    const unsubscribe = onSnapshot(
+      collection(db, "users", currentUserId, "friends"),
+      (snapshot) => {
+        data = snapshot.docs.map(doc => ({
+          uid: doc.id,
+          ...(doc.data() as Omit<User, 'uid'>)
+        }));
       }
+  );
 
+  return () => unsubscribe();
+}, [currentUserId]);
 
-
-    return (
-    <SafeAreaView edges={['top']} style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-            <ThemedView style={styles.mainContainer}>
-                <View style={styles.searchContainer}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder='Search friends'
-                        placeholderTextColor='gray'
-                        selectionColor='gray'
-                        value={search}
-                        onChangeText={(text) => filterData(text)}
-                    />
-                </View>
-
-                <LegendList
-                    data={filteredFriends}
-                    renderItem={renderItem}
-                    keyExtractor={(item) => item.id.toString()}
-                    recycleItems={true}
-                    maintainVisibleContentPosition            
-                />
-            </ThemedView>
-        </ScrollView>
-    </SafeAreaView>
-)}
+  return (
+      <SafeAreaView edges={['top']} style={{ flex: 1 }}>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+              <ThemedView style={styles.mainContainer}>
+                <UserSearch users={data}/>
+              </ThemedView>
+          </ScrollView>
+      </SafeAreaView>
+  )
+}
 
 
 
 
 
-const makeStyles = (colorScheme: any) => StyleSheet.create({
+
+const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     gap: vs(14),
-  },
-  searchContainer: {
-    paddingHorizontal: s(15),
-    paddingVertical: vs(15),
-    backgroundColor: 'transparent',
-    borderColor: '#7b68ee',
-    borderBottomWidth: 1,
-  },
-  input: {
-    height: vs(35),
-    borderColor: '#7b68ee',
-    borderWidth: 1,
-    paddingHorizontal: s(10),
-    borderRadius: 10,
-    color: colorScheme === 'dark' ? 'white' : 'black',
-    backgroundColor: 'transparent',
-  },
-  friendsContainer: {
-    paddingHorizontal: s(20),
-    paddingVertical: vs(4),
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: s(12),
   },
 })
