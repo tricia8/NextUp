66b3995cb9906ce216bbcf430563fad6a9b37813
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-18 23:30:55 +0800

    refactor(layout): move Stack.Screen for current-sublist to layout file in (main)

--- frontend/app/(main)/_layout.tsx
+++ frontend/app/(main)/_layout.tsx
@@ -4,13 +4,16 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { Redirect } from "expo-router";
 import { AuthContext } from "@/context/AuthContext";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { RFValue } from "react-native-responsive-fontsize";
+import { TouchableOpacity, View } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function MainLayout() {
   const { user, loading } = useContext(AuthContext);
@@ -35,6 +38,8 @@ export default function MainLayout() {
   // Handle unauthenticated access
   if (!user) return <Redirect href="/(auth)/login" />;
 
+  const router = useRouter();
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
@@ -47,6 +52,28 @@ export default function MainLayout() {
               fontWeight: "bold",
               fontSize: RFValue(26),
             },
+          }}
+        />
+        <Stack.Screen
+          name="(main)/current-sublist"
+          options={{
+            headerTitle: "",
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => router.push("/(main)/(tabs)/bucketlist")}
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 8,
+                }}
+              >
+                <MaterialIcons name="arrow-back" size={24} color="black" />
+
+                <View>
+                  <ThemedText>back to Bucket List</ThemedText>
+                </View>
+              </TouchableOpacity>
+            ),
           }}
         />
       </Stack>
