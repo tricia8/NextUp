0c6262ab2e607534125dc2eccb18a19f0bde48d4
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-18 18:57:57 +0800

    feat: add custom back button with expo routing for current-sublist screen
    - Back button navigates to bucketlist screen via router.push

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -4,19 +4,23 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TouchableOpacity, View } from "react-native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import { AuthProvider } from "@/context/AuthContext";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import FlashMessage from "react-native-flash-message";
+import { ThemedText } from "@/components/ThemedText";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
   });
+  const router = useRouter();
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -49,6 +53,28 @@ export default function RootLayout() {
             <Stack.Screen
               name="(main)/new-sublist"
               options={{ title: "New Sublist" }}
+            />
+            <Stack.Screen
+              name="(main)/current-sublist"
+              options={{
+                headerTitle: "",
+                headerLeft: () => (
+                  <TouchableOpacity
+                    onPress={() => router.push("/(main)/(tabs)/bucketlist")}
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      gap: 8,
+                    }}
+                  >
+                    <MaterialIcons name="arrow-back" size={24} color="black" />
+
+                    <View>
+                      <ThemedText>back to Bucket List</ThemedText>
+                    </View>
+                  </TouchableOpacity>
+                ),
+              }}
             />
             <Stack.Screen name="+not-found" />
           </Stack>
