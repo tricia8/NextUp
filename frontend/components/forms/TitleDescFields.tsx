a73f783bd73ab4ed751469926e04c5da9f048e26
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-19 17:01:43 +0800

    chore: relocate SublistField to @/components/forms/ for cleaner project structure and update relevant imports

---
+++ frontend/components/forms/TitleDescFields.tsx
@@ -0,0 +1,41 @@
+import { View } from "react-native";
+import SublistField from "./SublistField";
+
+interface TitleDescProps {
+  title: string;
+  description: string;
+  setTitle: (title: string) => void;
+  setDescription: (desc: string) => void;
+  lightLabelBg?: string;
+  darkLabelBg?: string;
+}
+
+export default function TitleDescFields({
+  title,
+  description,
+  setTitle,
+  setDescription,
+  lightLabelBg = "#a2e6ff",
+  darkLabelBg = "#141515",
+}: TitleDescProps) {
+  return (
+    <View style={{ gap: 10 }}>
+      <SublistField
+        label="Title"
+        onChangeText={(value) => setTitle(value)}
+        value={title}
+        lightLabelBg={lightLabelBg}
+        darkLabelBg={darkLabelBg}
+      />
+      <SublistField
+        label="Description"
+        onChangeText={(value) => setDescription(value)}
+        value={description}
+        multiline={true}
+        placeholder="Add details, timelines, or motivations..."
+        lightLabelBg={lightLabelBg}
+        darkLabelBg={darkLabelBg}
+      />
+    </View>
+  );
+}
