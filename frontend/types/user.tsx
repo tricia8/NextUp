90203ed8fc532d912ce03c25763626ba5eaaec4c
tricia <triciachew18@gmail.com>
2025-06-24 12:18:11 +0800

    refactor: update type User to have optional fields for better reusability and replace PartialUser with User

--- frontend/types/user.tsx
+++ frontend/types/user.tsx
@@ -1,9 +1,8 @@
 export type User = {
   uid: string;
   username: string;
-  email: string | null;
+  email?: string | null;
   photoUrl: string | null;
-  displayName: string;
-  bio: string;
-  category: string[];
+  displayName?: string | null;
+  bio?: string | null;
 };
