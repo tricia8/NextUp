fca93c5d1629d5e9f2f2585a52cc40173ae067cb
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-25 12:49:45 +0800

    feat: add 'collaborators' param to getEvents for cleaner DB logic

--- frontend/firebase/firestore.js
+++ frontend/firebase/firestore.js
@@ -397,7 +397,7 @@ export const deleteSubBucketList = async (userId, subBucketList) => {
 export const addEvent = async (
   userId,
   subBucketListId,
-  { title, description, categories, deadline }
+  { title, description, categories, deadline, collaborators }
 ) => {
   try {
     const eventData = {
@@ -405,6 +405,7 @@ export const addEvent = async (
       title,
       description,
       categories,
+      collaborators,
       isCompleted: false,
       createdAt: serverTimestamp(),
     };
@@ -498,6 +499,7 @@ export const getEvent = async (userId, subBucketListId, eventId) => {
       ? formatDisplayDate(data.deadline.toDate())
       : null;
     const isCompleted = data.isCompleted;
+    const collaborators = data.collaborators;
     const createdAt = data.createdAt.toDate(); // convert Firestore Timestamp to JS Date
     const createdAtFormatted = formatDisplayDate(createdAt);
 
@@ -507,6 +509,7 @@ export const getEvent = async (userId, subBucketListId, eventId) => {
       categories,
       deadline: deadlineFormatted, // could be null
       isCompleted,
+      collaborators,
       createdAt: createdAtFormatted,
     };
   } catch (error) {
@@ -571,7 +574,7 @@ export async function getAllEvents(db, uid, subBucketLists) {
   return allEvents;
 }
 
-// title, description, categories, deadline, isCompleted
+// title, description, categories, deadline, isCompleted, collaborators
 export const updateEvent = async (
   userId,
   subBucketListId,
