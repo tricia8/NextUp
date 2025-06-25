850626cb21817550b87aba401b10e30c59a815af
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-25 23:39:32 +0800

    feat: update DeleteModal UI with theme responsiveness,  improved positioning and text styles

--- frontend/components/DeleteModal.tsx
+++ frontend/components/DeleteModal.tsx
@@ -1,9 +1,16 @@
-import { Modal, Pressable, TouchableOpacity, View } from "react-native";
+import {
+  Modal,
+  Pressable,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from "react-native";
 import { ThemedView } from "./ThemedView";
 import { ThemedText } from "./ThemedText";
 import { RFValue } from "react-native-responsive-fontsize";
 import { Sublist } from "@/types/sublist";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { use } from "react";
 
 type DeleteModalProps = {
   modalVisible: boolean;
@@ -18,6 +25,8 @@ export default function DeleteModal({
   item,
   handleItemDelete,
 }: DeleteModalProps) {
+  const colorScheme = useColorScheme();
+
   if (!item) {
     return null;
   }
@@ -39,36 +48,50 @@ export default function DeleteModal({
             flex: 1,
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "rgba(0,0,0,0.3)",
+            backgroundColor: "rgba(0, 0, 0, 0.44)",
           }}
           onPress={() => setModalVisible(false)}
         >
-          <View
+          <ThemedView
             onStartShouldSetResponder={() => true}
             onTouchEnd={(e) => e.stopPropagation()}
             style={{
               width: "80%",
-              backgroundColor: "#fff",
               padding: 20,
               borderRadius: 10,
               elevation: 3,
               alignItems: "center",
+              backgroundColor: colorScheme === "dark" ? "#393939" : "#e5e5e5",
             }}
           >
-            <ThemedText style={{ fontSize: RFValue(15), marginBottom: 20 }}>
-              Are you sure you want to delete this sublist? Every goal in this
-              list, including completed goals, will be deleted as well. This
-              action cannot be undone."
+            <ThemedText
+              type="subtitle"
+              style={{
+                fontSize: RFValue(16),
+                marginBottom: 16,
+                textAlign: "center",
+              }}
+            >
+              Are you sure you want to delete this sublist?
+            </ThemedText>
+            <ThemedText
+              style={{
+                fontSize: RFValue(13),
+                marginBottom: 16,
+              }}
+            >
+              This will permanently delete all goals in this list, including
+              completed ones. This action cannot be undone.
             </ThemedText>
             <View
-              style={{ flexDirection: "row", justifyContent: "space-between" }}
+              style={{ flexDirection: "row", justifyContent: "space-around" }}
             >
               <TouchableOpacity
                 onPress={handleDelete}
                 style={{
-                  backgroundColor: "#ff4d4d",
+                  backgroundColor: "#ec4b6a",
                   margin: 10,
-                  paddingVertical: 10,
+                  padding: 10,
                   borderRadius: 5,
                   alignItems: "center",
                 }}
@@ -86,12 +109,12 @@ export default function DeleteModal({
                   alignItems: "center",
                 }}
               >
-                <ThemedText style={{ color: "#007bff", fontSize: RFValue(14) }}>
+                <ThemedText style={{ color: "#4aa1ff", fontSize: RFValue(14) }}>
                   Cancel
                 </ThemedText>
               </TouchableOpacity>
             </View>
-          </View>
+          </ThemedView>
         </Pressable>
       </Modal>
     </SafeAreaView>
