d1845d5a34e88c95e575043d74c3d24212c81b77
tricia <triciachew18@gmail.com>
2025-06-27 10:06:49 +0800

    refactor: created reusable component ProfilePic for conditional rendering of profile pic image and profile pic icon

---
+++ frontend/components/ProfilePic.tsx
@@ -0,0 +1,27 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { View, Image } from "react-native";
+import { ms } from "react-native-size-matters";
+
+type Props = {
+  imageUrl: string | null;
+  size: number;
+};
+
+export default function ProfilePic({ imageUrl, size }: Props) {
+  return (
+    <View>
+      {imageUrl ? (
+        <Image
+          source={{ uri: imageUrl }}
+          style={{
+            width: ms(size),
+            height: ms(size),
+            borderRadius: ms(size) / 2,
+          }}
+        />
+      ) : (
+        <FontAwesome name="user-circle-o" size={ms(size)} color="#7b68ee" />
+      )}
+    </View>
+  );
+}
