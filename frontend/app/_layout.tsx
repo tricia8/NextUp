e7020520078011b58062e52e57231842656b9a01
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-06-28 17:53:04 +0800

    refactor: render StatusBar component in Root layout for styling consistency and integrate useThemedUI hook

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -5,8 +5,12 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import FlashMessage from "react-native-flash-message";
 import { Slot } from "expo-router";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { StatusBar } from "expo-status-bar";
+import useThemedUI from "@/hooks/useThemedUI";
 
 export default function RootLayout() {
+  useThemedUI();
+
   return (
     <AuthProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
@@ -14,6 +18,7 @@ export default function RootLayout() {
           <SafeAreaProvider>
             <Slot />
             <FlashMessage position="top" />
+            <StatusBar style="auto" />
           </SafeAreaProvider>
         </BottomSheetModalProvider>
       </GestureHandlerRootView>
