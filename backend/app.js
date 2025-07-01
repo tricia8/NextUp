fd31a880acdfafbb112edc1b8e0b762fe365ce69
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-01 21:30:34 +0800

    feat: integrate API routes with /api prefix in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,17 @@ import express from "express";
 import morgan from "morgan";
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+import api from "./api";
 
 // Create Express app
 const app = express();
 app.use(morgan("dev")); // HTTP request logger to monitor API traffic
 app.use(express.json());
+app.use("/api", api); // All routes in api are prefixed with /api
 
 // Initialise app with admin privileges
 initializeApp({
   credential: applicationDefault(),
-  databaseURL: process.env.databaseURL,
 });
 
 const db = getFirestore();
@@ -27,3 +28,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+export default db;
