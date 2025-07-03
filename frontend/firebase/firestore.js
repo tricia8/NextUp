006df1537c7739312bc5ead7db06f893036dbb88
tricia <triciachew18@gmail.com>
2025-07-03 15:50:51 +0800

    fix: update "receiver" to "receiverId" for query in getFriendRequests

--- frontend/firebase/firestore.js
+++ frontend/firebase/firestore.js
@@ -754,7 +754,7 @@ export const getFriendRequests = async (userId) => {
   try {
     const q = query(
       collection(db, "friendRequests"),
-      where("receiver", "==", userId),
+      where("receiverId", "==", userId),
       where("status", "==", "pending"),
     );
 
