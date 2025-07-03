1ee00e7b53ee1380aa44678482dac6a5e6dbc464
tricia <triciachew18@gmail.com>
2025-07-03 18:33:04 +0800

    fix: update friendId to receiverId property

--- frontend/firebase/firestore.js
+++ frontend/firebase/firestore.js
@@ -859,7 +859,7 @@ export const getRequestInfo = async (requestId) => {
     return {
       id: requestSnapshot.id,
       senderId: requestData.senderId,
-      receiverId: requestData.friendId,
+      receiverId: requestData.receiverId,
       senderName: requestData.senderName,
       receiverName: requestData.receiverName,
       sentAt: requestData.sentAt,
