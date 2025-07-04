b5db7380132ad6c72daf0e4ff9fdfe40ef82e3df
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-04 20:05:05 +0800

    feat: improve response messages and add error logging
    - Update response messages for clarity and error tracing
    - Add console error log for easier debugging

--- backend/authenticate.js
+++ backend/authenticate.js
@@ -5,13 +5,17 @@ const verifyFirebaseToken = async (req, res, next) => {
   const header = req.headers.authorization;
   if (!header || !header.startsWith("Bearer ")) {
     res.statusMessage = "Unauthorized Header. Access Denied";
-    return res.status(401).send("Unauthorized Header. Access Denied");
+    return res
+      .status(401)
+      .send("Unauthorized Header (No Bearer). Access Denied");
   }
   const token = header.split(" ")[1];
 
   if (!token) {
     res.statusMessage = "Unauthorized Header. Access Denied";
-    return res.status(401).send("Unauthorized Header. Access Denied");
+    return res
+      .status(401)
+      .send("Unauthorized Header (Invalid Token). Access Denied");
   }
 
   try {
@@ -19,6 +23,7 @@ const verifyFirebaseToken = async (req, res, next) => {
     req.user = decodedToken.uid; // Attach uid to req.user
     next();
   } catch (error) {
+    console.error("Token verification error:", error);
     return res.status(401).send("Unauthorized Header. Access Denied");
   }
 };
