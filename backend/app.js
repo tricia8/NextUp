0cfdac8801c9543a528ec2655ab08874c00b6dfc
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-04 14:09:32 +0800

    fix: add .js extension to api import to resolve ESM module resolution error

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import morgan from "morgan";
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import api from "./api";
+import api from "./api.js";
 
 // Create Express app
 const app = express();
