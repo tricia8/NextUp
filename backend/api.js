00aba4b0a952c6ad276ede0207ba19a2c7c82420
tricia <triciachew18@gmail.com>
2025-07-05 17:42:57 +0800

    feat: set up routes for gemini api

--- backend/api.js
+++ backend/api.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import verifyFirebaseToken from "./authenticate.js";
 import bucketlistRoutes from "./routes/bucketlist.js";
+import geminiRoutes from "./routes/gemini.js";
 
 const router = Router();
 router.use(verifyFirebaseToken); // Authenticate all requests
 router.use("/", bucketlistRoutes);
+router.use("/gemini", geminiRoutes);
 
 export default router;
