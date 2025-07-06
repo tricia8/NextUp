3d2f5bdd8d2242d22595d4020b216ae8033075d9
tricia <triciachew18@gmail.com>
2025-07-06 10:44:35 +0800

    refactor: wrap in try catch block to handle errors

--- backend/routes/cloudinary.js
+++ backend/routes/cloudinary.js
@@ -4,37 +4,42 @@ import cloudinary from "../cloudinaryConfig.js";
 const router = Router();
 
 router.post("/signature", (req, res) => {
-  const timestamp = Math.floor(Date.now() / 1000);
-  const folder = req.body.folder;
-  const public_id = req.body.public_id;
+  try {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const folder = req.body.folder;
+    const public_id = req.body.public_id;
 
-  let overwrite = false;
+    let overwrite = false;
 
-  if (public_id.includes("profile_pic")) {
-    overwrite = true;
+    if (public_id.includes("profile_pic")) {
+      overwrite = true;
+    }
+
+    const paramsToSign = {
+      timestamp,
+      folder,
+      public_id,
+      overwrite,
+    };
+
+    const signature = cloudinary.utils.api_sign_request(
+      paramsToSign,
+      process.env.CLOUDINARY_API_SECRET
+    );
+
+    res.json({
+      timestamp,
+      signature,
+      folder,
+      public_id,
+      overwrite,
+      apiKey: process.env.CLOUDINARY_API_KEY,
+      cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    });
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-
-  const paramsToSign = {
-    timestamp,
-    folder,
-    public_id,
-    overwrite,
-  };
-
-  const signature = cloudinary.utils.api_sign_request(
-    paramsToSign,
-    process.env.CLOUDINARY_API_SECRET
-  );
-
-  res.json({
-    timestamp,
-    signature,
-    folder,
-    public_id,
-    overwrite,
-    apiKey: process.env.CLOUDINARY_API_KEY,
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  });
 });
 
 export default router;
