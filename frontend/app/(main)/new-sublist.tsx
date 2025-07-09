1dca33e940469b38a2a605a4c1191e05e919b0b7
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-10 00:13:24 +0800

    feat: fetch all users and add collaborator removal logic
    - Update useFocusEffect to fetch all users with cleanup

--- frontend/app/(main)/new-sublist.tsx
+++ frontend/app/(main)/new-sublist.tsx
@@ -18,7 +18,12 @@ import ShareListModal from "@/components/ShareListModal";
 import { Dimensions } from "react-native";
 import SublistField from "@/components/forms/SublistField";
 import AccessDropdownPicker from "@/components/forms/AccessDropdownPicker";
-import { createSubBucketList, getOwnerProfile } from "@/firebase/firestore";
+import {
+  addCollaborator,
+  createSubBucketList,
+  getAllUsers,
+  getOwnerProfile,
+} from "@/firebase/firestore";
 import { AuthContext } from "@/context/AuthContext";
 import { User } from "@/types/user";
 import LoadingScreen from "@/components/Loading";
@@ -42,11 +47,21 @@ export default function newSubList() {
   const [accessLevel, setAccessLevel] = useState("");
 
   // Invite collaborators
+  const [allUsers, setAllUsers] = useState<User[]>([]);
   const [sharedUids, setSharedUids] = useState<string[]>([]);
   const [collaborators, setCollaborators] = useState<User[]>([]);
+  const [invitedUsers, setInvitedUsers] = useState<string[]>([]);
+
+  // Remove collaborator
+  const onRemoveCollaborator = (uid: string) => {
+    setCollaborators((prev) => prev.filter((user) => user.uid !== uid));
+    setSharedUids((prev) => prev.filter((id) => id !== uid));
+    setInvitedUsers((prev) => prev.filter((id) => id !== uid));
+  };
 
   useFocusEffect(
     useCallback(() => {
+      let isActive = true; // to prevent state updates if the component is unmounted
       console.log("useFocusEffect triggered");
 
       const fetchOwner = async () => {
@@ -58,36 +73,55 @@ export default function newSubList() {
           const profile = (await getOwnerProfile(user.uid)) as User;
           console.log("Owner profile:", profile);
 
-          if (profile) {
+          const allUsers = (await getAllUsers()) as User[];
+          console.log("All users:", allUsers);
+
+          if (isActive) {
             setCollaborators([profile]);
             setSharedUids([profile.uid]);
+            setAllUsers(allUsers);
           }
-          console.log("collaborators", collaborators);
         } catch (error) {
           console.error("Failed to fetch owner profile:", error);
         }
       };
 
       fetchOwner();
+
+      return () => {
+        isActive = false; // cleanup function to prevent state updates if component is unmounted
+      };
     }, [user?.uid])
   );
 
   // Sublist submission
   const submit = async () => {
     try {
-      const sublistId = await createSubBucketList(user?.uid, {
+      const response = await createSubBucketList({
         title,
         description,
         accessLevel,
         collaborators: sharedUids,
-      });
+      }); // success, message, sublistId, sublistData
+
+      await Promise.all(
+        invitedUsers.map((uid) => {
+          addCollaborator(response.sublistId, uid);
+        })
+      ); // add invited users to collaborators
+
       // Delay to allow Firestore to propagate the new document
       await new Promise((res) => setTimeout(res, 300));
 
-      router.push({ pathname: "/(main)/[sublistId]", params: { sublistId } });
+      router.push({
+        pathname: "/(main)/[sublistId]",
+        params: {
+          sublistId: response.sublistId,
+        },
+      });
       showMessage({
         message: "Success!",
-        description: "New sublist added",
+        description: response.message,
         type: "success",
         statusBarHeight: StatusBar.currentHeight,
         floating: true,
@@ -157,6 +191,22 @@ export default function newSubList() {
             <ThemedText>Create Sublist</ThemedText>
           </TouchableOpacity>
         </ScrollView>
+        <View style={{ flex: 1 }}>
+          <ShareListModal
+            currentUid={user?.uid}
+            collaborators={collaborators} // User[]
+            setCollaborators={setCollaborators}
+            visible={modalVisible}
+            onClose={() => setModalVisible(false)}
+            setModalVisible={setModalVisible}
+            allUsers={allUsers}
+            sharedUids={sharedUids} // string[]
+            setSharedUids={setSharedUids}
+            invitedUids={invitedUsers} // string[]
+            setInvitedUids={setInvitedUsers}
+            onRemoveCollaborator={onRemoveCollaborator}
+          />
+        </View>
       </ThemedView>
     </SafeAreaView>
   );
