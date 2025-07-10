d9fa180dcad990b222336cd3115b7d45eac21d20
tricia <triciachew18@gmail.com>
2025-07-10 14:33:29 +0800

    chore: create babel.config.js, async-storage.js and install dependencies for testing with jest

---
+++ frontend/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    'babel-preset-expo',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': '.'
+        }
+      }
+    ]
+  ]
+};
