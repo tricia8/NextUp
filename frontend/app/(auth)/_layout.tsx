78dd8f7fe7374e45f9e197bee29b7faaef05ac1c
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-13 11:53:39 +0800

    chore: clean up imports, fix useEffect deps, and restore conditional check for navigation state key

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,16 +1,8 @@
-import {
-  Stack,
-  Redirect,
-  useRootNavigationState,
-  useRouter,
-  useNavigation,
-} from "expo-router";
+import { Stack, useRouter, useNavigation } from "expo-router";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "@/context/AuthContext";
-import useIsNavigationReady from "@/hooks/useIsNavigationReady";
 
 export default function AuthLayout() {
-  // const rootNavigationState = useRootNavigationState();
   const navigation = useNavigation();
   const navigationState = navigation.getState();
   const { user, loading } = useContext(AuthContext);
@@ -28,12 +20,12 @@ export default function AuthLayout() {
         router.replace("/(main)/(tabs)");
       }
     }
-  }, [user, loading /* rootNavigationState*/]);
+  }, [user, loading, navigationState]);
 
-  /* if (!rootNavigationState?.key) {
+  if (!navigationState?.key) {
     console.log("Waiting for navigation state...");
     return null; // Wait for navigation to be ready
-  } */
+  }
   if (loading) {
     console.log("Waiting for auth check...");
     return null; // Wait for auth check
