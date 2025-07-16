e7747bd52bcc4c61c384e97e012a4a996a486b00
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-16 17:54:22 +0800

    feat: add Post type definition

---
+++ frontend/types/post.tsx
@@ -0,0 +1,10 @@
+// Post type used in [goalId] screen
+export type Post = {
+  id: string; // postId
+  username: string;
+  profilePhotoUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  comment: string;
+  imageUrl: string;
+};
