c43d951abf56b33510f8a653cc8fbade0c52b5bd
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-17 16:03:09 +0800

    feat: add userId field to Post type for author identification

--- frontend/types/post.tsx
+++ frontend/types/post.tsx
@@ -1,6 +1,7 @@
 // Post type used in [goalId] screen
 export type Post = {
   id: string; // postId
+  userId: string; // userId of the post author
   username: string;
   profilePhotoUrl: string;
   createdAt: string;
