444450e63dfca16530de9f93cc1cb8e70709477b
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-17 17:28:20 +0800

    feat: update AddPostButton with optional onPress prop for external toggling
    - Allow AddPostButton to accept an optional onPress callback for integration with other components
    - Improves flexibility and enables cleaner UI interactions

--- frontend/components/AddPostButton.tsx
+++ frontend/components/AddPostButton.tsx
@@ -6,19 +6,24 @@ type AddPostButtonProps = {
   isVisible: boolean;
   setIsPostFormVisible: React.Dispatch<React.SetStateAction<boolean>>;
   isDark?: boolean;
+  onPress?: () => void;
 };
 
 export default function AddPostButton({
   isVisible,
   setIsPostFormVisible,
   isDark = false,
+  onPress = () => {},
 }: AddPostButtonProps) {
   const styles = getStyles(isDark);
   return (
     isVisible && (
       <TouchableOpacity
         style={styles.postButton}
-        onPress={() => setIsPostFormVisible(true)}
+        onPress={() => {
+          setIsPostFormVisible(true);
+          onPress();
+        }}
       >
         <ThemedText>Add post</ThemedText>
         <Ionicons
@@ -39,7 +44,7 @@ const getStyles = (isDark: boolean) =>
       justifyContent: "center",
       gap: 8,
       padding: 10,
-      backgroundColor: isDark ? "#64748c" : "rgba(216, 210, 213, 0.59)",
+      backgroundColor: isDark ? "#64748c" : "#cbe3df", // "#e2e8f0",
       elevation: 2,
       borderRadius: 12,
       borderWidth: 0,
