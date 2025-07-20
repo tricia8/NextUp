163fd13917f2570cc30041ad7071d9f96a37a651
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-20 10:22:38 +0800

    feat: create pickMultipleImages for selecting and uploading multiple images

--- frontend/cloudinary/pickimage.js
+++ frontend/cloudinary/pickimage.js
@@ -31,3 +31,41 @@ export async function pickImage(setImage) {
     console.log(error);
   }
 }
+
+// Multiple image picker
+export async function pickMultipleImages(setImages) {
+  try {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      alert("Permission to access media library is required!");
+      return null;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      base64: true,
+      mediaTypes: ["images"],
+      allowsMultipleSelection: true,
+      selectionLimit: 5,
+      presentationStyle: ImagePicker.PresentationStyle.FullScreen,
+      // allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImages(result.assets.map((asset) => asset.uri));
+      const base64Images = result.assets.map(
+        (asset) => `data:image/jpeg;base64,${asset.base64}`
+      );
+      return base64Images; // Cloudinary-ready
+    }
+
+    return null;
+  } catch (error) {
+    console.log(error);
+  }
+}
