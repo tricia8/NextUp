4294ce6225e30c62ebad2df84ac700a1d2c065cd
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-22 00:38:40 +0800

    feat: integrate post deletion and delete modal and improve post actions
    - Add trash bin icon and DeleteModal for post deletion with confirmation
    - Restrict post editing and deletion to owner only
    - Adjust post opacity to distinguish pending posts from stored posts
    - Update image rendering logic for compatibility with updated PostWithPending type

--- frontend/components/PostList.tsx
+++ frontend/components/PostList.tsx
@@ -3,11 +3,10 @@ import { LinearGradient } from "expo-linear-gradient";
 import {
   StyleSheet,
   View,
-  Image,
   TouchableOpacity,
   Dimensions,
+  StatusBar,
 } from "react-native";
-import { Post } from "@/types/post";
 import ProfilePic from "./ProfilePic";
 import { ThemedText } from "./ThemedText";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -16,12 +15,15 @@ import { RFValue } from "react-native-responsive-fontsize";
 import ViewMoreContent from "./ViewMoreContent";
 import { PostWithPending } from "@/types/postWithPending";
 import Carousel, { TAnimationStyle } from "react-native-reanimated-carousel";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { interpolate } from "react-native-reanimated";
 import PostImage from "./PostImage";
+import DeleteModal from "./DeleteModal";
+import { showMessage } from "react-native-flash-message";
 
 type PostListProps = {
   userId: string;
+  ownerId: string;
   posts: PostWithPending[];
   isDark?: boolean;
 };
@@ -30,10 +32,18 @@ const screenWidth = Dimensions.get("window").width;
 
 export default function PostList({
   userId,
+  ownerId,
   posts,
   isDark = false,
 }: PostListProps) {
-  const DATA: PostWithPending[] = [
+  const [modalVisible, setModalVisible] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState<PostWithPending | null>(
+    null
+  );
+  const heading = "Are you sure you want to delete this post?";
+  const body = "This action cannot be undone.";
+
+  /* const DATA: PostWithPending[] = [
     {
       id: "109384",
       userId: userId,
@@ -46,7 +56,47 @@ export default function PostList({
         "This is a sample post. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       imageUrls: ["https://picsum.photos/200/300"],
     },
-  ];
+  ]; */
+
+  const handleDeletePost = async (postId: string) => {
+    try {
+      console.log("deleting post");
+      await deletePost(postId);
+      console.log("deleted!");
+
+      /* updateData((prevSublists) =>
+            prevSublists.filter((list) => list.id !== sublist.id)
+          ); */
+      setModalVisible(false); // clsose modal after deletion
+      showMessage({
+        message: "Success",
+        description: "Post deleted!",
+        type: "success",
+        statusBarHeight: StatusBar.currentHeight,
+        floating: true,
+        icon: "success",
+        duration: 5000,
+      });
+    } catch (error) {
+      showMessage({
+        message: "Error",
+        description:
+          error instanceof Error ? error.message : "Error deleting post",
+        type: "danger",
+        statusBarHeight: StatusBar.currentHeight,
+        floating: true,
+        icon: "danger",
+        duration: 5000,
+      });
+    }
+  };
+
+  const onDeletePress = (post: PostWithPending) => {
+    () => {
+      setItemToDelete(post);
+      setModalVisible(true);
+    };
+  };
 
   const animationStyle: TAnimationStyle = useCallback((value: number) => {
     "worklet";
@@ -62,13 +112,13 @@ export default function PostList({
     };
   }, []);
 
-  const renderItem = ({ item }: { item: Post }) => {
+  const renderItem = ({ item }: { item: PostWithPending }) => {
     return (
       <LinearGradient
         colors={isDark ? ["#0f2027", "#664791"] : ["#77c4ff", "#a8b4ff"]}
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
-        style={styles.postContainer}
+        style={[styles.postContainer, item.isPending && { opacity: 0.5 }]}
       >
         <View
           style={{
@@ -99,7 +149,7 @@ export default function PostList({
             <ThemedText style={styles.text}>{item?.createdAt}</ThemedText>
           </View>
         </View>
-        {Array.isArray(item?.imageUrls) && item?.imageUrls.length > 0 && (
+        {Array.isArray(item?.images) && item?.images.length > 0 && (
           <>
             {/* <Image
               source={{ uri: item.imageUrls[0] }}
@@ -110,11 +160,11 @@ export default function PostList({
               autoPlay
               width={screenWidth * 0.7}
               height={240 * 0.7}
-              data={item?.imageUrls}
+              data={item?.images}
               snapEnabled
               mode="parallax"
               renderItem={({ index }) => (
-                <PostImage source={item?.imageUrls[index]} />
+                <PostImage source={item?.images[index].secureUrl} />
               )}
               onConfigurePanGesture={(gestureChain) =>
                 gestureChain.activeOffsetX([-10, 10])
@@ -124,24 +174,63 @@ export default function PostList({
           </>
         )}
         <ViewMoreContent content={item?.comment} />
-        <TouchableOpacity
-          onPress={() => console.log("Edit Post Pressed")}
-          hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
-          style={{ alignSelf: "flex-end" }}
-        >
-          <Feather name="edit-2" size={24} color={isDark ? "white" : "black"} />
-        </TouchableOpacity>
+        {ownerId === userId && (
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 10,
+              justifyContent: "flex-end",
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => console.log("Edit Post Pressed")}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            >
+              <Feather
+                name="edit-2"
+                size={24}
+                color={isDark ? "white" : "black"}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => onDeletePress(item)}
+              hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
+            >
+              <Feather
+                name="trash-2"
+                size={24}
+                color={isDark ? "white" : "black"}
+              />
+            </TouchableOpacity>
+          </View>
+        )}
       </LinearGradient>
     );
   };
 
   return (
-    <LegendList
-      data={posts}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-      contentContainerStyle={{ paddingBottom: 80 }}
-    />
+    <>
+      <LegendList
+        data={posts}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 80 }}
+      />
+      <DeleteModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        item={itemToDelete}
+        handleItemDelete={(item) => {
+          // type check to ensure item is a Post
+          if (item && "imageUrls" in item && "isPending" in item) {
+            handleDeletePost(item.id);
+          }
+        }}
+        heading={heading}
+        body={body}
+      />
+    </>
   );
 }
 
