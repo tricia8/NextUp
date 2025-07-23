045dbed4194845408393a08a1e5cdfbdb294461d
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-23 15:24:08 +0800

    feat: integrate Cloudinary URL optimization, resizeMethod prop and onError handler in PostImage

--- frontend/components/PostImage.tsx
+++ frontend/components/PostImage.tsx
@@ -1,5 +1,6 @@
 import Animated from "react-native-reanimated";
 import { type ImageStyle, type StyleProp, StyleSheet } from "react-native";
+import { getOptimizedCloudinaryUrl } from "@/utils/cloudinary";
 
 type PostImageProps = {
   source: string;
@@ -7,14 +8,20 @@ type PostImageProps = {
 };
 
 export default function PostImage({ source, style }: PostImageProps) {
+  const optimizedUri = getOptimizedCloudinaryUrl(
+    encodeURI(source),
+    720,
+    "auto"
+  );
+
   return (
-    <Animated.View style={{ flex: 1 }}>
-      <Animated.Image
-        style={[style, styles.container, { borderRadius: 15 }]}
-        source={{ uri: source }}
-        resizeMode="cover"
-      />
-    </Animated.View>
+    <Animated.Image
+      style={[style, styles.container, { borderRadius: 15 }]}
+      source={{ uri: optimizedUri }}
+      onError={(e) => console.log("Image failed to load", e.nativeEvent)}
+      resizeMode="cover"
+      resizeMethod="resize"
+    />
   );
 }
 
