560faa9eb52c5610143f0036825b6513b20922bd
Gracia Chew <159509493+gracias022@users.noreply.github.com>
2025-07-26 15:16:44 +0800

    feat: improve FilterPicker styling, fix overflow issue, and add closeSheet prop

--- frontend/components/FilterPicker.tsx
+++ frontend/components/FilterPicker.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { ThemedText } from "./ThemedText";
-import { Touchable, TouchableOpacity, View } from "react-native";
-import { Chip } from "react-native-paper";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import FilterChip from "./FilterChip";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export default function FilterPicker() {
+type FilterPickerProps = {
+  closeSheet: () => void;
+};
+
+export default function FilterPicker({ closeSheet }: FilterPickerProps) {
   // Filters
   const [owned, setOwned] = useState<boolean | undefined>(undefined);
   const [shared, setShared] = useState<boolean | undefined>(undefined);
@@ -16,85 +20,107 @@ export default function FilterPicker() {
   >(undefined);
 
   return (
-    <View style={{ gap: 10, flexShrink: 1 }}>
+    <View style={{ gap: 10 }}>
       <ThemedText type="subtitle">Filter Options</ThemedText>
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <ThemedText>Who owns it</ThemedText>
-        <FilterChip
-          onPress={() => {
-            setOwned(true);
-          }}
-          selected={owned === true}
-          label="Me"
-        />
-        <FilterChip
-          onPress={() => setOwned(false)}
-          selected={owned === false}
-          label="Not Me"
-        />
+      <View style={styles.chipsRow}>
+        <ThemedText style={styles.infoText}>Who owns it</ThemedText>
+        <View style={{ flexDirection: "row", gap: 5, flexWrap: "wrap" }}>
+          <FilterChip
+            onPress={() => {
+              setOwned(true);
+            }}
+            selected={owned === true}
+            label="Me"
+          />
+          <FilterChip
+            onPress={() => setOwned(false)}
+            selected={owned === false}
+            label="Not Me"
+          />
+        </View>
       </View>
 
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <ThemedText>Sharing status</ThemedText>
-        <FilterChip
-          onPress={() => setShared(true)}
-          selected={shared === true}
-          label="Shared"
-        />
-        <FilterChip
-          onPress={() => setShared(false)}
-          selected={shared === false}
-          label="Not Shared"
-        />
+      <View style={styles.chipsRow}>
+        <ThemedText style={styles.infoText}>Sharing status</ThemedText>
+        <View style={{ flexDirection: "row", gap: 5, flexWrap: "wrap" }}>
+          <FilterChip
+            onPress={() => setShared(true)}
+            selected={shared === true}
+            label="Shared"
+          />
+          <FilterChip
+            onPress={() => setShared(false)}
+            selected={shared === false}
+            label="Not Shared"
+          />
+        </View>
       </View>
 
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <ThemedText>Who can see it</ThemedText>
-        <FilterChip
-          onPress={() => setVisibility("private")}
-          selected={visibility === "private"}
-          label="Only Me"
-        />
-        <FilterChip
-          onPress={() => setVisibility("friends")}
-          selected={visibility === "friends"}
-          label="Friends"
-        />
+      <View style={styles.chipsRow}>
+        <ThemedText style={styles.infoText}>Who can see it</ThemedText>
 
-        <FilterChip
-          onPress={() => setVisibility("everyone")}
-          selected={visibility === "everyone"}
-          label="Everyone"
-        />
+        <View style={{ flexDirection: "row", gap: 5, flexWrap: "wrap" }}>
+          <FilterChip
+            onPress={() => setVisibility("private")}
+            selected={visibility === "private"}
+            label="Only Me"
+          />
+          <FilterChip
+            onPress={() => setVisibility("friends")}
+            selected={visibility === "friends"}
+            label="Friends"
+          />
+
+          <FilterChip
+            onPress={() => setVisibility("everyone")}
+            selected={visibility === "everyone"}
+            label="Everyone"
+          />
+        </View>
       </View>
 
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <ThemedText>Progress status</ThemedText>
-        <FilterChip
-          onPress={() => setProgressStatus("Completed")}
-          selected={progressStatus === "Completed"}
-          label="Completed"
-        />
-        <FilterChip
-          onPress={() => setProgressStatus("In Progress")}
-          selected={progressStatus === "In Progress"}
-          label="In Progress"
-        />
-        <FilterChip
-          onPress={() => setProgressStatus("Getting Started")}
-          selected={progressStatus === "Getting Started"}
-          label="Getting Started"
-        />
+      <View style={styles.chipsRow}>
+        <ThemedText style={styles.infoText}>Progress status</ThemedText>
+        <View style={{ flexDirection: "row", gap: 5, flexWrap: "wrap" }}>
+          <FilterChip
+            onPress={() => setProgressStatus("Completed")}
+            selected={progressStatus === "Completed"}
+            label="Completed"
+          />
+          <FilterChip
+            onPress={() => setProgressStatus("In Progress")}
+            selected={progressStatus === "In Progress"}
+            label="In Progress"
+          />
+          <FilterChip
+            onPress={() => setProgressStatus("Getting Started")}
+            selected={progressStatus === "Getting Started"}
+            label="Getting Started"
+          />
+        </View>
       </View>
 
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={closeSheet}>
           <ThemedText>Cancel</ThemedText>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={closeSheet}>
           <ThemedText>Apply Filters</ThemedText>
         </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  chipsRow: {
+    flexDirection: "row",
+    gap: 8,
+    flexWrap: "wrap",
+    alignItems: "center",
+  },
+  infoText: {
+    fontStyle: "italic",
+    fontSize: RFValue(11.5),
+  },
+});
